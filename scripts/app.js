9944f9b869f0697fccdc45dee526f78c7eee3ec4
David Morisset <dmorisset@gmail.com>
2016-05-17 21:25:39 +0100

    adds a widget carousel

--- scripts/app.js
+++ scripts/app.js
@@ -825,48 +825,106 @@ var carousel = function($collection) {
  ***************************************************************************/
 
 
-var sitesCarousel = carousel($("#carousel-sites .carousel-sites-item"));
+
+/* ABASTRACTING LINKS AND CAROUSELS
+************************************************************************/
+
+var moveL = function(event, collection, $linkCollection) {
+    event.stopPropagation();
+    event.preventDefault();
+    collection.moveLeft();
+    $linkCollection.removeClass("selected");
+    $linkCollection.eq(collection.currentItemNumber).addClass("selected");
+};
+
+var moveR = function(event, collection, $linkCollection) {
+    event.stopPropagation();
+    event.preventDefault();
+    collection.moveRight();
+    $linkCollection.removeClass("selected");
+    $linkCollection.eq(collection.currentItemNumber).addClass("selected");
+};
+
+var changeItem = function(event, collection, $linkCollection) {
+    event.stopPropagation();
+    event.preventDefault();
+    var $nextImage = $(event.target);
+    collection.changeItem($nextImage.data("num"));
+    $nextImage.removeClass("selected")
+    $linkCollection.removeClass("selected");
+    $nextImage.addClass("selected");
+};
+
+
+
+
+
+/* SITES CAROUSEL
+************************************************************************/
+
+var sitesCarousel = carousel($("#carousel-sites .carousel-item"));
 var $sitesLinks = $("#sites-list a");
 
 /* Events Listeners for sites carousel */
 $("#sites-carousel_left").click(function(event) {
-	event.stopPropagation();
-    event.preventDefault();
-	sitesCarousel.moveLeft();
-    $sitesLinks.removeClass("selected");
-    $sitesLinks.eq(sitesCarousel.currentItemNumber).addClass("selected");
+    moveL(event, sitesCarousel, $sitesLinks);
 });
 
 $("#carousel-sites").on( "swipeleft", function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    sitesCarousel.moveLeft();
-    $sitesLinks.removeClass("selected");
-    $sitesLinks.eq(sitesCarousel.currentItemNumber).addClass("selected");
+    moveL(event, sitesCarousel, $sitesLinks);
 });
 
 $("#sites-carousel_right").click(function(event) {
-	event.stopPropagation();
-    event.preventDefault();
-	sitesCarousel.moveRight();
-    $sitesLinks.removeClass("selected");
-    $sitesLinks.eq(sitesCarousel.currentItemNumber).addClass("selected");
+	moveR(event, sitesCarousel, $sitesLinks);
 });
 
 $("#carousel-sites").on( "swiperight", function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    sitesCarousel.moveRight();
-    $sitesLinks.removeClass("selected");
-    $sitesLinks.eq(sitesCarousel.currentItemNumber).addClass("selected");
+    moveR(event, sitesCarousel, $sitesLinks);
 });
 
 $sitesLinks.click(function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    var $nextImage = $(event.target);
-    sitesCarousel.changeItem($nextImage.data("site-num"));
-    $nextImage.removeClass("selected")
-    $sitesLinks.removeClass("selected");
-    $nextImage.addClass("selected");
+    changeItem(event, sitesCarousel, $sitesLinks);
 });
+
+
+/* WIDGETS CAROUSEL
+************************************************************************/
+
+var widgetsCarousel = carousel($("#carousel-widgets .carousel-item"));
+var $widgetsLinks = $("#widgets-list a");
+
+/* Events Listeners for sites carousel */
+$("#widgets-carousel_left").click(function(event) {
+    moveL(event, widgetsCarousel, $widgetsLinks);
+});
+
+$("#carousel-widgets").on( "swipeleft", function(event) {
+    moveL(eevent, widgetsCarousel, $widgetsLinks);
+});
+
+$("#widgets-carousel_right").click(function(event) {
+    moveR(event, widgetsCarousel, $widgetsLinks);
+});
+
+$("#carousel-widgets").on( "swiperight", function(event) {
+    moveR(event, widgetsCarousel, $widgetsLinks);
+});
+
+$widgetsLinks.click(function(event) {
+    changeItem(event, widgetsCarousel, $widgetsLinks);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
