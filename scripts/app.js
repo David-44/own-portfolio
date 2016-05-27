dba490c5d10daf80ac32207ca3d07641f3ee8183
David Morisset <dmorisset@gmail.com>
2016-05-27 22:24:48 +0100

    adds a fade-out overlay to the work panel

--- scripts/app.js
+++ scripts/app.js
@@ -1073,11 +1073,15 @@ var contactImage = $("#contact-image");
 
 $(document).ready(pageUpdate);
 
+function makeThingsAppear() {
+    if (wintop >= 20) {
+    	$("#work-overlay").addClass("fade-out-slow");
+    }
+}
 
 
 
-
-
+$(window).on("scroll", makeThingsAppear)
 
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,15 @@
 
 $(document).ready(pageUpdate);
 
+function makeThingsAppear() {
+    if (wintop >= 20) {
+    	$("#work-overlay").addClass("fade-out-slow");
+    }
+}
 
 
 
-
-
+$(window).on("scroll", makeThingsAppear)
 
 
 
