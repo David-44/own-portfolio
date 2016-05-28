cb22d08923471f6ca77f1323169fdde4e0c6ae0a
David Morisset <dmorisset@gmail.com>
2016-05-28 23:00:40 +0100

    the about part now looks cool

--- scripts/app.js
+++ scripts/app.js
@@ -276,6 +276,45 @@ $(window).scroll(pageUpdate);
 $(window).resize(pageUpdate);
 
 /***************************************************************************
+ * OVERLAY REMOVER
+ ***************************************************************************/
+
+ // Note : This function makes use of the variable wintop, defined in the indicator.js file
+
+function removeOverlay(trigger, $overlay1, $overlay2) {
+	if (wintop >= trigger) {
+    	$overlay1.addClass("fade-out-slow");
+    	if ($overlay2) {
+    	    $overlay2.remove();
+        }
+    	setTimeout(function(){
+            $overlay1.remove();
+    	}, 1000);
+    }
+}
+
+function makeThingsAppear() {
+	if ($(window).width() >= 800) {
+	    removeOverlay(22, $("#work-overlay"), $("#widgets-overlay"));
+	    removeOverlay(55, $("#perso-overlay"), $("#about-overlay"));
+	    removeOverlay(87, $("#contact-overlay"));
+    } else {
+        removeOverlay(15, $("#work-overlay"));
+        removeOverlay(34, $("#widgets-overlay"));
+        removeOverlay(53, $("#perso-overlay"));
+        removeOverlay(72, $("#about-overlay"));
+        removeOverlay(89, $("#contact-overlay"));
+    }
+    
+    if (wintop >= 89) {
+    	$(window).off("scroll", makeThingsAppear);
+    }
+}
+
+
+
+$(window).on("scroll", makeThingsAppear);
+/***************************************************************************
  * EASTER
  ***************************************************************************/
 
@@ -1073,41 +1112,27 @@ var contactImage = $("#contact-image");
 
 $(document).ready(pageUpdate);
 
-// Note : This function makes use of the variable wintop, defined in the indicator.js file
+$("#about-link").click(function(event) {
+    event.preventDefault();
+    var ref = $(this).attr("href");
+    var background = $("<div>").css({
+    	"width": "100%",
+    	"height": "100%",
+    	"position": "fixed",
+    	"top": "0",
+    	"left": "0",
+    	"z-index": "999",
+    	"background-color": "rgba(0, 0, 0, 0.8)"
+    });
+    var certif = $("<img>").attr("src", ref).addClass("certificate").fadeIn(500);
+    $("body").append(background).append(certif);
 
-function removeOverlay(trigger, $overlay1, $overlay2) {
-	if (wintop >= trigger) {
-    	$overlay1.addClass("fade-out-slow");
-    	if ($overlay2) {
-    	    $overlay2.remove();
-        }
-    	setTimeout(function(){
-            $overlay1.remove();
-    	}, 1000);
-    }
-}
-
-function makeThingsAppear() {
-	if ($(window).width() >= 800) {
-	    removeOverlay(22, $("#work-overlay"), $("#widgets-overlay"));
-	    removeOverlay(55, $("#perso-overlay"), $("#about-overlay"));
-	    removeOverlay(87, $("#contact-overlay"));
-    } else {
-        removeOverlay(15, $("#work-overlay"));
-        removeOverlay(34, $("#widgets-overlay"));
-        removeOverlay(53, $("#perso-overlay"));
-        removeOverlay(72, $("#about-overlay"));
-        removeOverlay(89, $("#contact-overlay"));
-    }
-    
-    if (wintop >= 89) {
-    	$(window).off("scroll", makeThingsAppear);
-    }
-}
-
-
-
-$(window).on("scroll", makeThingsAppear);
+    background.click(function(event) {
+        event.preventDefault();
+        certif.fadeOut();
+        background.fadeOut();
+    });
+});
 
 
 
