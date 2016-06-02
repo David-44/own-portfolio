be68d136af561f7cf80528ad5be74ef98f82caf9
David Morisset <dmorisset@gmail.com>
2016-06-02 21:30:10 +0100

    adds proper banner and header for browsers with no animations

--- scripts/app.js
+++ scripts/app.js
@@ -46,6 +46,37 @@ if (!dummy.style.backgroundImage) {
 
 
 
+// Testing for animations
+// Borrowed from https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Detecting_CSS_animation_support
+var animation = false,
+    animationstring = 'animation',
+    keyframeprefix = '',
+    domPrefixes = 'Webkit'.split(' '),
+    pfx  = '',
+    elm = document.createElement('div');
+
+if( elm.style.animationName !== undefined ) { animation = true; }    
+
+if( animation === false ) {
+  for( var i = 0; i < domPrefixes.length; i++ ) {
+    if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+      pfx = domPrefixes[ i ];
+      animationstring = pfx + 'Animation';
+      keyframeprefix = '-' + pfx.toLowerCase() + '-';
+      animation = true;
+      break;
+    }
+  }
+}
+
+if (!animation) {
+	$("html").addClass("no-animations");
+}
+
+
+
+
+
 
 /***************************************************************************
  * CAROUSEL CONSTRUCTOR
